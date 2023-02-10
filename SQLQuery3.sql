ff523c7c2b7be47f99c6a793b8a4aa49a3430b05
Максим Берзегов <103566620+Kirikay1@users.noreply.github.com>
2023-02-10 16:10:03 +0300

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,35 @@
+GO
+--1
+SELECT Snum
+FROM STD
+WHERE Kolvo > SOME (SELECT MAX(Kolvo)
+					FROM STD 
+					WHERE Snum = 'S1')
+GO
+--2
+SELECT Snum
+from S
+WHERE S.Status < SOME  (SELECT S.Status
+						FROM S
+						WHERE Snum = 'S1')
+GO
+--3
+CREATE TABLE TovarTemp(
+	Id INT PRIMARY KEY IDENTITY,
+	Tname nvarchar(50) NOT NULL
+	)
+DELETE FROM TovarTemp
+INSERT INTO TovarTemp
+SELECT T.Tname
+FROM T
+WHERE T.Price > 1000 AND
+T.EdIzm = 'пачка'
+GO
+--4
+UPDATE T 
+SET Price = Price - ((Price / 100) * 15)
+WHERE T.EdIzm = 'пачка, 10 кг'
+GO
+--5
+DELETE FROM S
+WHERE Status < 20
