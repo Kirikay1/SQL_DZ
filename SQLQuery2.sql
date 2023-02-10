e0d292928ad73185095c341e447462e7e13658bb
Максим Берзегов <103566620+Kirikay1@users.noreply.github.com>
2023-02-10 13:57:06 +0300

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -5,31 +5,24 @@ FROM S
 WHERE S.Status BETWEEN 50 AND 100
 GO
 --2
-SELECT S.Sname
+SELECT COUNT(S.Sname) AS Moscow_deliveries
 FROM S
 WHERE (S.City='Москва')
 GO
 --3
-SELECT T.Price
+SELECT SUM(T.Price)
 FROM T, STD
 WHERE (T.Tnum = STD.Tnum) AND
 (STD.Dnum = 'D1')
 GO
 --4
 SELECT S.Sname, AVG(T.Price * STD.Kolvo) AS AVG_PRICE
-FROM T,S, STD, D
+FROM T, S, STD, D
 WHERE (S.Snum = STD.Snum) AND
 (T.Tnum = STD.Tnum)
 GROUP BY S.Sname
 GO
---5 ОДИН ИЗ НИХ
-SELECT D.City, S.City
-FROM S, STD, D
-WHERE (S.Snum = STD.Snum) AND
-(D.Dnum = STD.Dnum)
-
-GO
---5 ОДИН ИЗ НИХ, СКОРЕЕЕ ВСЕГО ЭТОТ
+--5
 SELECT S.City
 FROM  S, D, STD, T
 WHERE (S.Snum = STD.Snum) AND
